2c86547f811666937c6ff7df3da357d00c1364bb
lamlib <lamlib>
2026-02-12 19:51:33 +0700

    refactor: restructure class utilities and enhance styling with new grid and layout components

--- script/build-css.js
+++ script/build-css.js
@@ -28,7 +28,6 @@ app.addCSSFile("./src/dropdown/style.css");
 app.addCSSFile("./src/model/style.css");
 app.addCSSFile("./src/table/style.css");
 app.addCSSFile("./src/form/style.css");
-app.addCSSFile("./src/grid/style.css");
 app.addCSSFile("./src/list-group/style.css");
 app.addCSSFile("./src/navbar/style.css");
 app.addCSSFile("./src/pagination/style.css");
@@ -38,9 +37,21 @@ app.addCSSFile("./src/tooltip/style.css");
 
 app.addCSSFile("./src/class/margin/style.css");
 app.addCSSFile("./src/class/padding/style.css");
+app.addCSSFile("./src/class/grid/style.css");
 app.addCSSFile("./src/class/text/style.css");
 app.addCSSFile("./src/class/list/style.css");
-app.addCSSFile("./src/class/style.css");
+app.addCSSFile("./src/class/display/style.css");
+app.addCSSFile("./src/class/layout/style.css");
+app.addCSSFile("./src/class/flex/style.css");
+app.addCSSFile("./src/class/position/style.css");
+app.addCSSFile("./src/class/rounded/style.css");
+app.addCSSFile("./src/class/size/style.css");
+app.addCSSFile("./src/class/background/style.css");
+app.addCSSFile("./src/class/overflow/style.css");
+app.addCSSFile("./src/class/opacity/style.css");
+app.addCSSFile("./src/class/border/style.css");
+app.addCSSFile("./src/class/shadow/style.css");
+app.addCSSFile("./src/class/transition/style.css");
 
 app.build();
 
